cf07ea17d855c97f249db5074b2a23e060a94407
kalvin <cuswrthkalvin@gamil.com>
2026-05-12 19:31:10 -0400

    initial commit

---
+++ SkillHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkillHub
+//
+//  Created by Kalvin Cusworth on 2026-05-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SkillHubTests/SkillHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkillHubTests.swift
+//  SkillHubTests
+//
+//  Created by Kalvin Cusworth on 2026-05-12.
+//
+
+import Testing
+@testable import SkillHub
+
+struct SkillHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
