b2760de6ae5b6f35b49f8ce513d89df625496b83
Kalvin Cusworth <cuswortk@oa-lc-17.ddi.sheridanc.on.ca>
2026-05-26 21:20:50 -0400

    made delete account useble

--- SkillHub/Privacy.swift
+++ SkillHub/Privacy.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class PrivacyViewController: UIViewController {
     
@@ -15,5 +16,34 @@ class PrivacyViewController: UIViewController {
         let createVC = storyboard.instantiateViewController(withIdentifier: "change")
         createVC.modalPresentationStyle = .fullScreen
         present(createVC, animated: true)
+    }
+    
+    @IBAction func deleteAccountTapped(_ sender: Any) {
+        let name: String? = UserDefaults.standard.string(forKey: "username")
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request: NSFetchRequest<User> = User.fetchRequest()
+        request.predicate = NSPredicate(format: "name == %@", name!)
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if let user = results.first {
+                context.delete(user)
+            }
+            
+            try context.save()
+            
+            UserDefaults.standard.set(nil, forKey: "username")
+            UserDefaults.standard.set(nil, forKey: "userEmail")
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let createVC = storyboard.instantiateViewController(withIdentifier: "SignIn")
+            createVC.modalPresentationStyle = .fullScreen
+            present(createVC, animated: true)
+            
+        } catch let error {
+            print("Failed to delete user: \(error)")
+        }
     }
 }
