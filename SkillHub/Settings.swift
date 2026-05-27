a37c9f9549e395db5475954c4efe6b5ffde5049b
Kalvin Cusworth <cuswortk@oa-lc-17.ddi.sheridanc.on.ca>
2026-05-26 21:12:41 -0400

    made logout useble

--- SkillHub/Settings.swift
+++ SkillHub/Settings.swift
@@ -23,4 +23,14 @@ class SettingsViewController: UIViewController {
         createVC.modalPresentationStyle = .fullScreen
         present(createVC, animated: true)
     }
+    
+    @IBAction func logOutTapped(_ sender: Any) {
+        UserDefaults.standard.set(nil, forKey: "username")
+        UserDefaults.standard.set(nil, forKey: "userEmail")
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let createVC = storyboard.instantiateViewController(withIdentifier: "SignIn")
+        createVC.modalPresentationStyle = .fullScreen
+        present(createVC, animated: true)
+    }
 }
