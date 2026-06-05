c9577237e819ece525118c35a619de4d6af16679
komikronsgit <cusworthkalvin@gmail.com>
2026-06-04 21:52:50 -0400

    key test

--- SkillHub/Config.swift
+++ SkillHub/Config.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct Config {
 
-    static let openAIKey = ""
-    static let supabaseAnonKey = ""
+    static let openAIKey = "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"
+    static let supabaseAnonKey = "sb_secret_P9h8iJ7cShWOgcEwGUin2Q_wBNpKsQI"
 }
