4774646b5542a438782cae7fc1f828aecc00e306
youngjebae <youngje1027@gmail.com>
2026-06-05 00:04:03 -0400

    Hide Create Skill Post navigation title while keeping back button visible

--- SkillHub/SkillPost.swift
+++ SkillHub/SkillPost.swift
@@ -18,6 +18,32 @@ class SkillPostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureNavigationBar()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        configureNavigationBar()
+    }
+
+    // MARK: - Navigation Bar
+    func configureNavigationBar() {
+        // Navigation bar는 숨기지 않음
+        // 그래야 Back 버튼이 계속 보임
+        navigationController?.setNavigationBarHidden(false, animated: false)
+
+        // 가운데 title 제거
+        self.title = ""
+        self.navigationItem.title = ""
+
+        // Storyboard에서 자동으로 잡힌 title도 안 보이게 막음
+        self.navigationItem.titleView = UIView()
+
+        // Large title 방지
+        self.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = false
     }
 
     @IBAction func postSkillTapped(_ sender: UIButton) {
@@ -50,7 +76,15 @@ class SkillPostViewController: UIViewController {
 
             await MainActor.run {
                 print("Skill post submitted")
-                dismiss(animated: true)
+
+                // Push로 들어온 화면이면 이전 화면으로 pop
+                if let navigationController = self.navigationController,
+                   navigationController.viewControllers.count > 1 {
+                    navigationController.popViewController(animated: true)
+                } else {
+                    // Modal로 열린 경우 fallback
+                    self.dismiss(animated: true)
+                }
             }
         }
     }
@@ -66,4 +100,4 @@ class SkillPostViewController: UIViewController {
 
         present(alert, animated: true)
     }
-}
+} 
